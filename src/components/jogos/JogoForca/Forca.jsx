4be13c48d93cab1da6b1a43e9c611560824caa83
igorheije <heije.tomori2014@outlook.com>
2020-12-21 13:40:12 -0300

    forca

--- src/components/jogos/JogoForca/Forca.jsx
+++ src/components/jogos/JogoForca/Forca.jsx
@@ -2,36 +2,64 @@ import React from 'react';
 
 import Main from '../../template/Main';
 import './style.css';
+import './Forca.css';
 
 // import Boneco from './Boneco'
 import Letras from './Letras';
 import './jogo da forca';
 
 export default (props) => {
-  const [letras, setLetras] = React.useState(['a', 'b']);
+  const [letras, setLetras] = React.useState([]);
+  const [error, setError] = React.useState('');
+  const [corretas, setCorretas] = React.useState([]);
+  const [erros, setErros] = React.useState(0);
 
-  function validarLetra() {
-    const palavra = 'palavra';
+  const palavra = 'palavra';
+  const regexLetras = /^[a-z]+$/i;
 
-    console.log(palavra);
+  function validarLetra(letra) {
+    if (erros < 7) {
+      if ([...palavra].includes(letra)) {
+        setCorretas([...corretas, letra.toLocaleUpperCase()]);
+      } else {
+        setErros(erros + 1);
+      }
+    }
   }
 
   function jogar(e) {
     e.preventDefault();
-    if (!letras.includes(e.target[0].value)) {
-      setLetras([...letras, e.target[0].value]);
-      e.target[0].value = '';
-      e.target[0].focus();
-      validarLetra();
+    const input = e.target[0];
+    if (erros === 7) return;
+
+    if (regexLetras.test(input.value)) {
+      if (letras.includes(input.value.toLocaleUpperCase())) {
+        setError('Letra já inclusa');
+      } else {
+        validarLetra(input.value);
+        setLetras([...letras, input.value.toLocaleUpperCase()]);
+        input.value = '';
+        input.focus();
+
+        setError('');
+      }
+    } else {
+      setError('Apenas Letras');
     }
   }
+
   React.useEffect(() => {
-    console.log(letras);
-  }, [letras]);
+    if (erros === 7) setError('Você Perdeu');
+    console.log(corretas);
+  }, [erros]);
 
   return (
     <Main icon="paw" title="Jogo da Forca" subtitle="Sobre">
-      <Letras />
+      <div className="palavra">
+        {[...palavra.toLocaleUpperCase()].map((l, i) => {
+          return <div key={i}> {corretas.includes(l) ? l : ''} </div>;
+        })}
+      </div>
       {/* <Boneco/> */}
       <p>
         Letras digitadas :
@@ -43,6 +71,8 @@ export default (props) => {
         <input id="letra" type="text" maxLength="1" />
         <button>Jogar</button>
       </form>
+      <br />
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </Main>
   );
 };
